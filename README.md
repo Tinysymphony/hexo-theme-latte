9455b19567513652c534d2b8ffeb3a2370820e5c
Tinysymphony <zjutiny@gmail.com>
2016-05-27 21:05:04 +0800

    Modify theme files of Yilia

---
+++ README.md
@@ -0,0 +1,6 @@
+hexo-theme-latte
+================
+
+Latte 是由 Hexo 主题 [Yilia](https://github.com/litten/hexo-theme-yilia) 修改而来的, 感谢原作者 Litten ~
+
+修改了一些页面问题，为自己定制了点样式，目前用于自己的[博客](http://www.wytiny.com)
---
+++ source/js/pc.js
@@ -0,0 +1,111 @@
+define([], function () {
+
+    var Tips = (function () {
+
+        var $tipBox = $(".tips-box");
+
+        return {
+            show: function () {
+                $tipBox.removeClass("hide");
+            },
+            hide: function () {
+                $tipBox.addClass("hide");
+            },
+            init: function () {
+
+            }
+        }
+    })();
+
+	var hashString = function(str) {
+		var hash = 0,
+			len = str.length,
+			i, chr;
+		for(i = 0; i < len; i++) {
+			chr = str.charCodeAt(i);
+			hash = ((hash << 5) - hash) + chr;
+			hash |= 0;
+		}
+		return Math.abs(hash);
+	}
+
+    var resetTags = function () {
+        var tags = $(".tagcloud a");
+        tags.css({"font-size": "12px"});
+        for (var i = 0, len = tags.length; i < len; i++) {
+            var num = hashString(tags.eq(i).html()) % 7 + 1;
+            tags[i].className = "";
+            tags.eq(i).addClass("color" + num);
+        }
+    }
+
+    var slide = function (idx) {
+        var $wrap = $(".switch-wrap");
+        $wrap.css({
+            "transform": "translate(-" + idx * 100 + "%, 0 )"
+        });
+        $(".icon-wrap").addClass("hide");
+        $(".icon-wrap").eq(idx).removeClass("hide");
+    }
+
+    var bind = function () {
+        var switchBtn = $("#myonoffswitch");
+        var tagcloud = $(".second-part");
+        var navDiv = $(".first-part");
+        switchBtn.click(function () {
+            if (switchBtn.hasClass("clicked")) {
+                switchBtn.removeClass("clicked");
+                tagcloud.removeClass("turn-left");
+                navDiv.removeClass("turn-left");
+            } else {
+                switchBtn.addClass("clicked");
+                tagcloud.addClass("turn-left");
+                navDiv.addClass("turn-left");
+                resetTags();
+            }
+        });
+
+        var timeout;
+        var isEnterBtn = false;
+        var isEnterTips = false;
+
+        $(".icon").bind("mouseenter", function () {
+            isEnterBtn = true;
+            Tips.show();
+        }).bind("mouseleave", function () {
+            isEnterBtn = false;
+            setTimeout(function () {
+                if (!isEnterTips) {
+                    Tips.hide();
+                }
+            }, 100);
+        });
+
+        $(".tips-box").bind("mouseenter", function () {
+            isEnterTips = true;
+            Tips.show();
+        }).bind("mouseleave", function () {
+            isEnterTips = false;
+            setTimeout(function () {
+                if (!isEnterBtn) {
+                    Tips.hide();
+                }
+            }, 100);
+        });
+
+        $(".tips-inner li").bind("click", function () {
+            var idx = $(this).index();
+            slide(idx);
+            Tips.hide();
+        });
+    }
+
+
+    return {
+        init: function () {
+            resetTags();
+            bind();
+            Tips.init();
+        }
+    }
+});
